7fc1e14c0fde34e3695a70342bc86bf28246c638
Strega's Gate <55062873+STREGAsGate@users.noreply.github.com>
2021-09-20 22:31:59 -0400

    Add .zero static variable.

---
+++ Sources/Raylib/Swifty/Vector3.swift
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2021 Dustin Collins (Strega's Gate)
+ * All Rights Reserved.
+ * Licensed under Apache License v2.0
+ *
+ * Find me on https://www.YouTube.com/STREGAsGate, or social media @STREGAsGate
+ */
+
+import _RaylibC
+
+public extension Vector3 {
+    static var zero: Vector3 {Vector3(x: 0, y: 0, z: 0)}
+}
