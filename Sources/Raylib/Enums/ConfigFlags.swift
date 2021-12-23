1224e85df2a94149e3262483225f3dd5f8673ea1
Strega's Gate <55062873+STREGAsGate@users.noreply.github.com>
2021-12-22 21:41:39 -0500

    Make ConfigFlags members public

--- Sources/Raylib/Enums/ConfigFlags.swift
+++ Sources/Raylib/Enums/ConfigFlags.swift
@@ -13,33 +13,33 @@ public struct ConfigFlags: OptionSet, Equatable, RawRepresentable {
     public let rawValue: UInt32
     
     /// Set to try enabling V-Sync on GPU
-    static let vsyncHint                = ConfigFlags(0x00000040)
+    public static let vsyncHint                = ConfigFlags(0x00000040)
     /// Set to run program in fullscreen
-    static let fullscreenMode           = ConfigFlags(0x00000002)
+    public static let fullscreenMode           = ConfigFlags(0x00000002)
     /// Set to allow resizable window
-    static let windowResizable          = ConfigFlags(0x00000004)
+    public static let windowResizable          = ConfigFlags(0x00000004)
     /// Set to disable window decoration (frame and buttons)
-    static let windowUndecorated        = ConfigFlags(0x00000008)
+    public static let windowUndecorated        = ConfigFlags(0x00000008)
     /// Set to hide window
-    static let windowsHidden            = ConfigFlags(0x00000080)
+    public static let windowsHidden            = ConfigFlags(0x00000080)
     /// Set to minimize window (iconify)
-    static let windowMinimized          = ConfigFlags(0x00000200)
+    public static let windowMinimized          = ConfigFlags(0x00000200)
     /// Set to maximize window (expanded to monitor)
-    static let windowMaximized          = ConfigFlags(0x00000400)
+    public static let windowMaximized          = ConfigFlags(0x00000400)
     /// Set to window non focused
-    static let windowUnfocused          = ConfigFlags(0x00000800)
+    public static let windowUnfocused          = ConfigFlags(0x00000800)
     /// Set to window always on top
-    static let windowTopmost            = ConfigFlags(0x00001000)
+    public static let windowTopmost            = ConfigFlags(0x00001000)
     /// Set to allow windows running while minimized
-    static let windowAlwaysRun          = ConfigFlags(0x00000100)
+    public static let windowAlwaysRun          = ConfigFlags(0x00000100)
     /// Set to allow transparent framebuffer
-    static let windowTransparent        = ConfigFlags(0x00000010)
+    public static let windowTransparent        = ConfigFlags(0x00000010)
     /// Set to support HighDPI
-    static let windowHighDPI            = ConfigFlags(0x00002000)
+    public static let windowHighDPI            = ConfigFlags(0x00002000)
     /// Set to try enabling MSAA 4X
-    static let Fmsaa4xHint              = ConfigFlags(0x00000020)
+    public static let Fmsaa4xHint              = ConfigFlags(0x00000020)
     /// Set to try enabling interlaced video format (for V3D)
-    static let interlacedHint           = ConfigFlags(0x00010000)
+    public static let interlacedHint           = ConfigFlags(0x00010000)
     
     public init(_ rawValue: UInt32) {
         self.rawValue = rawValue
