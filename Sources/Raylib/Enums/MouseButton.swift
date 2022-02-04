2ba7d13fb2e74694e46d737219eecafd83fead1d
Alejandro Martínez <alexito4@gmail.com>
2022-02-04 19:36:33 +0100

    Added all buttons to MouseButton enum

--- Sources/Raylib/Enums/MouseButton.swift
+++ Sources/Raylib/Enums/MouseButton.swift
@@ -8,7 +8,11 @@
 
 // Mouse buttons
 public enum MouseButton: Int32 {
-    case left   = 0
-    case right  = 1
-    case middle = 2
+    case left       = 0 // Mouse button left
+    case right      = 1 // Mouse button right
+    case middle     = 2 // Mouse button middle (pressed wheel)
+    case side       = 3 // Mouse button side (advanced mouse device)
+    case extra      = 4 // Mouse button extra (advanced mouse device)
+    case forward    = 5 // Mouse button fordward (advanced mouse device)
+    case back       = 6 // Mouse button back (advanced mouse device)
 }
